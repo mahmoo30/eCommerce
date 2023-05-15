7fbeb15c6033fc291a055e15499866ed234c8f19
mahmoo30 <wasif.96@gmail.com>
2023-05-14 19:17:46 -0500

    imported models into index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -12,6 +12,31 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+
+// Define relationships between models
+
+// A Product belongs to a Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id', // The foreign key in the Product model
+  });
+  
+  // A Product belongs to many Tags through the ProductTag model
+  Product.belongsToMany(Tag, {
+    through: ProductTag, // The intermediate model
+    foreignKey: 'product_id', // The foreign key in the Product model
+  });
+  
+  // A Tag belongs to many Products through the ProductTag model
+  Tag.belongsToMany(Product, {
+    through: ProductTag, // The intermediate model
+    foreignKey: 'tag_id', // The foreign key in the Tag model
+  });
+  
+  // A Category has many Products
+  Category.hasMany(Product, {
+    foreignKey: 'category_id', // The foreign key in the Product model
+  });
+
 module.exports = {
   Product,
   Category,
